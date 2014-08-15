d864eb467d2a7a1106cd94158aa17183d4cce79e
Nick Youngblut <ndy2@cornell.edu>
2014-08-15 07:22:25 -0600

    140815: init

--- bin/gasic_seqDB_batch.py
+++ bin/gasic_seqDB_batch.py
@@ -8,35 +8,38 @@ indexFile -- bowtie2 index file
 metaFile -- metadata file with mg-rast or sra metagenome sequence links
 
 Usage:
-  gasic_seqDB_batch.py <indexFile> <metaFile>
+  gasic_seqDB_batch.py [options] <indexFile> <metaFile>
   gasic_seqDB_batch.py -h | --help
   gasic_seqDB_batch.py --version
 
 Options:
-  -h --help     Show this screen.
+  --seqDB=<seqDB>       Sequence database name ('MGRAST' or 'SRA'). [default: MGRAST]
+  --stages=<stages>...  MG-RAST processing stages to attempt download of. 
+  Each in list will be tried until successful download. [default: 200,150]
   --version     Show version.
-  --speed=<kn>  Speed in knots [default: 10].
-  --moored      Moored (anchored) mine.
-  --drifting    Drifting mine.
+  -h --help     Show this screen.
 """
 
 from docopt import docopt
+import re
 
 if __name__ == '__main__':
     args = docopt(__doc__, version='0.1')
+    args['--seqDB'] = args['--seqDB'].upper()
+    args['--stages'] = re.split(r'\s*,\s*', args['--stages'])
 
 
 #--- Package import ---#
 import os
 import sys
+import tempfile
+import shutil
 
 scriptDir = os.path.dirname(__file__)
 libDir = os.path.join(scriptDir, '../lib/')
 sys.path.append(libDir)
 libDir = os.path.join(scriptDir, '../lib/gasic-r16/')
 sys.path.append(libDir)
-
-#import core.tools
 
 import gasicBatch.objects as gobj
 
@@ -54,18 +57,44 @@ fileExists(args['<metaFile>'])
 # bowtie2 in path?
 
 
-
 #--- Main ---#
-
 
 # reading 
 ## metaFile
-metaFileO = gobj.metaFile(fileName=args['<metaFile>'], seqDB='MGRAST', sep='\t')
+if args['--seqDB'] == 'MGRAST':
+    metaFileO = gobj.MetaFile_MGRAST(fileName=args['<metaFile>'], 
+                                     stages=args['--stages'], 
+                                     sep='\t')
+elif args['--seqDB'] == 'SRA':
+    metaFileO = gobj.MetaFile_SRA(fileName=args['<metaFile>'],
+                                  sep='\t')
 
 
 # each metagenome
-for ID in metaFileO.iterByRow():
-    # downloading     
-    metaFileO.download( ID )
+for row in metaFileO.iterByRow():
 
+    # making temp directory and chdir
+    tmpdir = tempfile.mkdtemp()
+    
+    try:
+        # downloading     
+        metaFileO.download( ID=row['ID'] )
+         
+        # determine read stats
+        metaFileO.getReadStats( fileName=metaFileO.outFile, fileFormat='fasta')
+
+        # read mapping with gasic
+        
+
+        # read simulation and mapping
+
+        # creating similarity matrix
+
+        # similarity correction
+
+    finally:
+        # clean up tempdir
+        shutil.rmtree(tmpdir)
+
+    # debug
     sys.exit()
--- lib/gasicBatch/objects.py
+++ lib/gasicBatch/objects.py
@@ -1,19 +1,22 @@
 import sys
 import os
 import re
-#import urllib
-#import urllib2
-import requests
+import gzip
+import zlib
 
+from Bio import SeqIO
+import scipy
+import requests
 import pandas as pd
 
 
-class metaFile(object):
-    """metaFile object class
+
+class MetaFile(object):
+    """metadata table file object class
     """
     
     def __init__(self, fileName=None, fileObject=None, 
-                 seqDB='MGRAST', stages=[150, 100], **pandas_kwargs):
+                 stages=None, **pandas_kwargs):
         """Loading file as pandas dataFrame. Checking for necessary columns
         
         Kwargs:
@@ -25,47 +28,73 @@ class metaFile(object):
         # attributes
         self.fileName = fileName
         self.fileObject = fileObject
-        self.seqDB = seqDB
-        self.stages = [str(x) for x in stages]
-        
+        try:
+            self.stages = [str(x) for x in stages]
+        except TypeError:
+            self.stages = None
+        self.pd_kwargs = pandas_kwargs
 
-        # checking seqDB
-        if self.seqDB is not 'MGRAST' and self.seqDB is not 'SRA':
-            raise IOError( 'seqDB must be "MGRAST" or "SRA"' )
+        # loading MetaFile file
+        self.loadFile()        
 
-        # loading file
-        sep = '\t'
-        if fileName is not None:
-            self._tbl = pd.read_csv(fileName, **pandas_kwargs)
-        elif fileObject is not None:            
-            self._tbl = pd.read_csv(fileObject, **pandas_kwargs)
+
+    def loadFile(self):        
+        """Loading file as pandas DataFrame """
+        if self.fileName is not None:
+            self._tbl = pd.read_csv(self.fileName, **self.pd_kwargs)
+        elif self.fileObject is not None:            
+            self._tbl = pd.read_csv(self.fileObject, **self.pd_kwargs)
         else:
             raise IOError( 'Provide either fileName or fileObject' )
 
-        # check for needed columns
-        ## MGRAST
-        ### columns needed: id
-        if self.seqDB is 'MGRAST':
-            try:
-                self._tbl['id']
-            except:
-                raise IOError( 'MGRAST metaFile does not have "id" column' )
+    @classmethod
+    def gunzip(cls, inFile, outFile=None):
+        """gunzip of a provided file"""
+        if outFile is None:
+            outFile, ext = os.path.splitext(inFile)
+        print outFile
 
-        ## SRA
-        ### columns needed: ftp
-        elif self.seqDB is 'SRA':
-            try:
-                self._tbl['ftp']
-            except:
-                raise IOError( 'SRA metaFile does not have "ftp" column' )
+    def getReadStats(self, fileName, fileFormat='fasta'):
+        """Loading read file and getting stats:
+        Length distribution: min, max, mean, median, stdev
+        
+        Output:
+        readStats attribute -- dict of stats (eg., 'mean' : mean_value)
+        """
 
-        ## seqDB not recognized
-        else:
-            raise IOError( 'seqDB "{0}" is not recognized'.format(self.seqDB) )
+        seqLens = []
+        for seq_record in SeqIO.parse(fileName, fileFormat):
+            seqLens.append( len(seq_record) )
+        self.readStats = { 
+            'min' : scipy.min(seqLens),
+            'mean' : scipy.mean(seqLens),
+            'median' : scipy.median(seqLens),
+            'max' : scipy.max(seqLens),
+            'stdev' : scipy.std(seqLens)
+            }
+            
+
+class MetaFile_MGRAST(MetaFile):
+    """Subclass of MetaFile class for importing mgrast metafile """
+
+    def __init__(self, fileName=None, fileObject=None, 
+                 stages=[150, 100], **pandas_kwargs):
+        MetaFile.__init__(self, fileName=fileName, fileObject=fileObject, 
+                          stages=stages, **pandas_kwargs)
+        self.checkFile()
+    
+
+    def checkFile(self):
+        """Checking for needed columns in mg-rast metadata file"""
+        try:
+            self._tbl['id']
+        except:
+            raise IOError( 'MGRAST metaFile does not have "id" column' )
 
 
     def iterByRow(self):
-        """Return iterator for processing table by row
+        """Return iterator for processing table by row for mg-rast metadata table
+        requires metaFile in pandas dataframe class
         
         Return:
         metagenome_id
@@ -82,34 +111,85 @@ class metaFile(object):
             else:
                 metagenome_id = m.group(1)
 
+            # determining sequencing platform
+            platform = self.getPlatform(row)
+
             # yield
-            yield metagenome_id
-            
+            yield {'ID' : metagenome_id, 'platform' : platform}
+    
+
+    def getPlatform(self, row):
+        """Determining sequencing platform from metadata file
+
+        Column(s) to check:
+        seq_method
+        
+        Args:
+        row -- row of pandas DataFrame
+        """
+        
+        # check for column
+        try: 
+            row['seq_method']
+        except KeyError as err:
+            raise type(err)(err.message + '. "seq_meth" column not in metadata file!')
+
+        # determine sequencing platform
+        platform = None
+        if re.search(r'454|pyro', row['seq_method'], flags=re.I) is not None:
+            platform = '454'
+        elif re.search( r'illumina', row['seq_method'], flags=re.I) is not None:
+            platform = 'illumina'
+        elif re.search( r'sanger', row['seq_method'], flags=re.I) is not None:
+            platform = 'sanger'
+        # return
+        return platform
+
 
     def download(self, ID):
-        """MG-RAST API request for downloading metagenome
+        """MG-RAST API request for downloading metagenome reads.
+        Reads are downloaded as gzipped files.
 
         Kwargs:
-        ID -- metagenome ID
+        ID -- mg-rast metagenome ID
+
+        Attrib edit:
+        outFile -- string with downloaded file name
         """        
+
+        # input check
+        if ID is None:
+            raise TypeError('ID cannot be None type')
         
         # trying each stage
         for stage in self.stages:
             # initialize url
             url = 'http://api.metagenomics.anl.gov/1/download/'
-            url = url + ID
-            url = url + '?stage={0}'.format(stage)            
+            url = url + ID + '?file={0}.1'.format(stage)            
 
             # send request to mgrast
+            sys.stderr.write( 'For ID: "{0}", trying stage: "{1}"\n'.format(ID, stage) )
             sys.stderr.write( 'Sending request: "{0}"\n'.format(url) )
             req = requests.get(url)
-            
-            ## need to use curl get request to download as compressed file
+            sys.stderr.write(' Request status: {0}\n'.format(str(req.status_code)))
+            if req.status_code != 200:   # try next stage 
+                sys.stderr.write('  Request != 200. Skipping!\n')
+                continue
 
-            print(req.status_code)
-            #urllib.urlretrieve(url, ID)
-            #response = urllib2.urlopen(url)
-            #print(response)
-            #html = response.read()
-            #print(url)
+            # writing content to file
+            outFile = ID + '_stage' + stage + '.fasta'
+            d = zlib.decompressobj(16+zlib.MAX_WBITS)
+            with open(outFile, 'wb') as fd:
+                for chunk in req.iter_content(1000):
+                    #fd.write(chunk)
+                    fd.write( d.decompress( chunk) )
+            sys.stderr.write(' File written: {0}\n'.format(outFile))
+
+            # checking that content was written
+            ## else: try next stage
+            if os.stat(outFile)[6] != 0:
+                self.outFile = outFile
+            else:
+                sys.stderr.write(' Requested content was empty! Skipping!\n')
+                continue                      
         
